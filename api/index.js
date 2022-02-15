dbc967e579bf9d6843a07e13a8b438bfd0221ee8
gustavomaciel <Gfromwork011@gmail.com>
2022-02-15 17:38:25 -0300

    Implementacao de backend, e metodo de login

---
+++ api/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(express.json({}));
+app.use(cors());
+
+app.post("/login", function (req, res) {
+  console.log(req.body);
+  if (
+    req.body.email != "macielgustavo323@gmail.com" ||
+    req.body.senha != "qweasd123"
+  ) {
+    return res.status(400).json({ status: "Email ou senha Invalida !" });
+  }
+  res.json({ status: "Login efetuado com êxito", code: 200 });
+});
+app.listen(3000);
--- js/index.js
+++ js/index.js
@@ -20,5 +20,25 @@ function togglePassword() {
 
 document.getElementById("btn-login").addEventListener("click", function (e) {
   e.preventDefault();
-  alert("Login Efetuado!");
+  //alert("Login Efetuado!");
+  const form = document.getElementById("form");
+  const dados = new FormData(form);
+  const obj = { email: dados.get("email"), senha: dados.get("senha") };
+  fetch("http://localhost:3000/login", {
+    body: JSON.stringify(obj),
+    method: "post",
+    headers: {
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+  })
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (res) {
+      console.log(res, "caui aq");
+      alert(res.status);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 });
