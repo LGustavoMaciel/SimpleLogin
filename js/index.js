0f872c6fa99e153ae0e69d7aa4c103e1f809cb55
gustavomaciel <Gfromwork011@gmail.com>
2022-02-15 09:27:49 -0300

    Correcao do atributo Mostrar senha

--- js/index.js
+++ js/index.js
@@ -1,16 +1,20 @@
-function showPassword() {
+function togglePassword() {
   const eye = document.getElementById("eye");
   const eyeSlash = document.getElementById("eye-slash");
   const fieldPassword = document.getElementById("field-password");
+
+  eye.classList.toggle("active");
 
   if (eye.style.display === "none") {
     eye.style.display = "block";
     eyeSlash.style.display = "none";
     fieldPassword.type = "text";
+    console.log("caiu aq 2");
   } else {
     eye.style.display = "none";
     eyeSlash.style.display = "block";
     fieldPassword.type = "password";
+    console.log("caiu aq");
   }
 }
 
