d580996707ae0b5d5af123f292937ca81584f718
gustavomaciel <Gfromwork011@gmail.com>
2022-02-24 15:18:11 -0300

    Alteracao e inicio da home page

--- js/homepage.js
+++ js/homepage.js
@@ -1,6 +1,6 @@
 document.getElementById("btn-exit").addEventListener("click", function (e) {
-  setTimeout(function () {
-    location.replace("./index.html");
-  }, 1000);
-  toastr.warning("Saindo !");
+    setTimeout(function () {
+        location.replace("./index.html");
+    }, 1000);
+    toastr.warning("Saindo !");
 });
