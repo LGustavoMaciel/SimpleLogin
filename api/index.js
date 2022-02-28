5810c373982d701eb039e9e4c4d8d902841427b9
Luis Gustavo <Gfromwork011@gmail.com>
2022-02-28 17:33:50 -0300

    atualizacao de estilos na tela de login

--- api/index.js
+++ api/index.js
@@ -6,13 +6,13 @@ app.use(express.json({}));
 app.use(cors());
 
 app.post("/login", function (req, res) {
-  console.log(req.body);
-  if (
-    req.body.email != "macielgustavo323@gmail.com" ||
-    req.body.senha != "qweasd123"
-  ) {
-    return res.status(400).json({ status: "Email ou senha Invalida !" });
-  }
-  res.json({ status: "Login efetuado com êxito", code: 200 });
+    console.log(req.body);
+    if (
+        req.body.email != "email@email.com.br" ||
+        req.body.senha != "qweasd123"
+    ) {
+        return res.status(400).json({ status: "Email ou senha Invalida !" });
+    }
+    res.json({ status: "Login efetuado com êxito", code: 200 });
 });
 app.listen(3000);
